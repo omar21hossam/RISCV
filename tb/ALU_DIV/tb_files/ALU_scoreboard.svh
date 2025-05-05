4a19b1f3463c46ac8cd7ff5b63c7ac8ef738326d
Osama Mohamed Elsheikh <osama.mohamed.elshiekh@gmail.com>
2025-05-05 03:58:13 +0300

    Update ALU_scoreboard.svh

--- tb/ALU_DIV/tb_files/ALU_scoreboard.svh
+++ tb/ALU_DIV/tb_files/ALU_scoreboard.svh
@@ -33,249 +33,267 @@ class alu_scoreboard extends uvm_scoreboard;
 //==============================================================================
 //Description: model_function
 //==============================================================================
-    function void Model(alu_seq_item trans);
+ function void Model(alu_seq_item trans);
        alu_seq_item trans_model = alu_seq_item::type_id::create("trans_model");
        if(!trans.rst_n)begin
             trans.result_o = 0;
             trans.comparison_result_o = 0;
-            trans.ready_o = 0;
+            trans.ready_o = 1;
        end 
        else begin
+			trans_model.operand_a_i = trans.operand_a_i;
+			trans_model.operand_b_i = trans.operand_b_i;
+			trans_model.operator_i = trans.operator_i;
+			trans_model.enable_i = trans.enable_i;
+			trans_model.ex_ready_i = trans.ex_ready_i;
+			trans_model.rst_n = trans.rst_n;
             case(trans.operator_i)
             ALU_ADD:begin
                 trans_model.result_o = trans.operand_a_i + trans.operand_b_i;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
             end
             ALU_SUB:begin
                 trans_model.result_o = trans.operand_a_i - trans.operand_b_i;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
             end
             ALU_ADDU:begin
                 trans_model.result_o = trans.operand_a_i + trans.operand_b_i;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_SUBU:begin
                  trans_model.result_o = trans.operand_a_i - trans.operand_b_i;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_XOR:begin
                 trans_model.result_o = trans.operand_a_i ^ trans.operand_b_i;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_OR:begin
                 trans_model.result_o = trans.operand_a_i | trans.operand_b_i;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_AND:begin
                 trans_model.result_o = trans.operand_a_i & trans.operand_b_i;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_SRA:begin
-                trans_model.result_o = trans.operand_a_i >>> trans.operand_b_i[3:0];
+              trans_model.result_o = $signed(trans.operand_a_i) >>> trans.operand_b_i[4:0];
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_SRL:begin
-                trans_model.result_o = trans.operand_a_i >> trans.operand_b_i[3:0];
+              trans_model.result_o = trans.operand_a_i >> trans.operand_b_i[4:0];
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_SLL:begin
-                trans_model.result_o = trans.operand_a_i << trans.operand_b_i[3:0];
+              trans_model.result_o = trans.operand_a_i << trans.operand_b_i[4:0];
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
             end
             ALU_LTS:begin
                 if($signed(trans.operand_a_i ) < $signed(trans.operand_b_i))begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_LTU:begin
                 if(trans.operand_a_i  < trans.operand_b_i)begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end 
             end
             ALU_LES:begin
                 if($signed(trans.operand_a_i ) <= $signed(trans.operand_b_i))begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_LEU:begin
                  if(trans.operand_a_i  <= trans.operand_b_i)begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end 
             end
             ALU_GTS:begin
                 if($signed(trans.operand_a_i ) > $signed(trans.operand_b_i))begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_GTU:begin
                 if(trans.operand_a_i  > trans.operand_b_i)begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end 
             end
             ALU_GES:begin
                 if($signed(trans.operand_a_i ) >= $signed(trans.operand_b_i))begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_GEU:begin
                 if(trans.operand_a_i  >= trans.operand_b_i)begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end 
             end
             ALU_EQ:begin
                 if(trans.operand_a_i  == trans.operand_b_i)begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_NE:begin
                 if(trans.operand_a_i  != trans.operand_b_i)begin
                 trans_model.result_o = 32'hffff_ffff;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_SLTS:begin
                 if($signed(trans.operand_a_i ) < $signed(trans.operand_b_i))begin
                 trans_model.result_o = 1;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_SLTU:begin
                  if(trans.operand_a_i < trans.operand_b_i)begin
                 trans_model.result_o = 1;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end 
             end
             ALU_SLETS:begin
                 if($signed(trans.operand_a_i ) <= $signed(trans.operand_b_i))begin
                 trans_model.result_o = 1;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 trans_model.op_type = 1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
             end
             ALU_SLETU:begin
                 if(trans.operand_a_i <= trans.operand_b_i)begin
                 trans_model.result_o = 1;
                 trans_model.comparison_result_o  = 1'b1;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end
                 else begin
                 trans_model.result_o = 0;
                 trans_model.comparison_result_o  = 1'b0;
-                trans_model.ready_o = 1'b0;
+                trans_model.ready_o = 1'b1;
                 end 
             end
             //**Osama Alzero code**//
             ALU_DIVU:begin
-            end
+				trans_model.result_o =  trans.operand_b_i / trans.operand_a_i ;
+                trans_model.ready_o = 1'b1;
+				trans_model.comparison_result_o  = 1'b0;
+			end
             ALU_DIV:begin
+				trans_model.result_o =  $signed(trans.operand_b_i) / $signed(trans.operand_a_i) ;
+                trans_model.ready_o = 1'b1;
+				trans_model.comparison_result_o  = 1'b0;
             end
             ALU_REMU:begin
+				trans_model.result_o =  trans.operand_b_i % trans.operand_a_i ;
+                trans_model.ready_o = 1'b1;
+				trans_model.comparison_result_o  = 1'b0;
             end
             ALU_REM:begin
+				trans_model.result_o = $signed(trans.operand_b_i) % $signed(trans.operand_a_i) ;
+                trans_model.ready_o = 1'b1;
+				trans_model.comparison_result_o  = 1'b0;
             end
             default:begin
                 `uvm_fatal(get_type_name(), $sformatf("ALU Scoreboard: Invalid operation code:%p",trans.operator_i));
@@ -297,7 +315,7 @@ function void check_phase(uvm_phase phase);
         // $stop;
      if(seq_item_mon_out_q.size() > 0) begin
         if(seq_item_mon_out_q[0].compare(seq_item_model_q[0]))begin
-            `uvm_info(get_type_name(), $sformatf("ALU Scoreboard: Comparison result: %s", "PASS"), UVM_MEDIUM);
+            `uvm_info(get_type_name(), $sformatf("ALU Scoreboard: Comparison result: %s", "PASS"), UVM_NONE);
         end
         else begin
            // seq_item_model_q[0].print();
@@ -306,7 +324,7 @@ function void check_phase(uvm_phase phase);
             
            // seq_item_mon_out_q[0].print();
             `uvm_error(get_type_name(), $sformatf("ALU Scoreboard: Comparison result: %s with ID :%0d", "FAIL",trans_ID));
-             $stop;
+            // $stop;
            
         end
     end
