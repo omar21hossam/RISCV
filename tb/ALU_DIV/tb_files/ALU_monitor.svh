e261c23544aebee503a7d3009eccd59c015207e8
Osama Mohamed Elsheikh <osama.mohamed.elshiekh@gmail.com>
2025-05-06 18:39:18 +0300

    Monitoring timing signal

--- tb/ALU_DIV/tb_files/ALU_monitor.svh
+++ tb/ALU_DIV/tb_files/ALU_monitor.svh
@@ -44,6 +44,7 @@ task run_phase(uvm_phase phase);
       seq_item.operand_a_i = vif.operand_a_i;
       seq_item.operand_b_i = vif.operand_b_i;
       seq_item.ex_ready_i  = vif.ex_ready_i;
+      seq_item.testing_time = $realtime;
       `uvm_info(get_type_name(), $sformatf("ALU Monitor: Sampling inputs: %s", seq_item.convert2string()), UVM_MEDIUM);
       analysis_port.write(seq_item);
       `uvm_info(get_type_name(), $sformatf("send input transaction to scoreboard"), UVM_MEDIUM);
@@ -63,6 +64,7 @@ task run_phase(uvm_phase phase);
        seq_item.result_o = vif.result_o;
        seq_item.ready_o = vif.ready_o;
        seq_item.comparison_result_o = vif.comparison_result_o;
+       seq_item.testing_time = $realtime;
 	 // end
        `uvm_info(get_type_name(), $sformatf("ALU Monitor: Sampling outputs: %s", seq_item.convert2string()), UVM_MEDIUM);
         analysis_port.write(seq_item);
