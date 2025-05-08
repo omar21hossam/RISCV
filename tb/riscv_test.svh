b8b829a61e0349aad4535229a5678a7569cf1bcd
omar21hossam <62779504+omar21hossam@users.noreply.github.com>
2025-05-08 14:53:17 +0300

    added the virtual sequence in the test

--- tb/riscv_test.svh
+++ tb/riscv_test.svh
@@ -4,6 +4,7 @@ class riscv_base_test extends uvm_test;
   riscv_env             env;
   riscv_sequenceb       seq;
   fetch_config_obj      cfg;
+  vsequ1              v_seq;
 
   virtual riscv_intf    riscv_intf_;
   virtual interface_clk interface_clk_;
@@ -15,6 +16,7 @@ class riscv_base_test extends uvm_test;
     super.build_phase(phase);
     env = riscv_env::type_id::create("env", this);
     cfg = fetch_config_obj::type_id::create("cfg", this);
+    v_seq = vsequ1::type_id::create("v_seq", this);
     seq = riscv_sequenceb::type_id::create("seq");
     if (seq == null) `uvm_info("build_phase", "sequence = null ", UVM_LOW);
 
@@ -39,7 +41,7 @@ class riscv_base_test extends uvm_test;
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
     phase.raise_objection(this);
-    seq.start(env.fetch_agnt.sqr);
+    v_seq.start(env.vseqr);
     phase.phase_done.set_drain_time(this, 5000ns);
     phase.drop_objection(this);
   endtask
