2e54b32f824771575e7f04fa3a9bad7d9d3f4680
AhmedAmrAbdellatif1 <140100601+AhmedAmrAbdellatif1@users.noreply.github.com>
2025-05-10 15:32:44 +0300

    UPDATE SOMETHING :(

--- tb/mul/files.f
+++ tb/mul/files.f
@@ -1,6 +1,5 @@
 ../../rtl/package/cv32e40p_pkg.sv
 ../../rtl/cv32e40p_mult.sv
-riscv_pkg.sv
 package_uvm.sv
 mul_interface.sv
 mul_tb.sv
--- tb/riscv_main_sequencer.svh
+++ tb/riscv_main_sequencer.svh
@@ -1,9 +1,13 @@
 
-
 class riscv_main_sequencer extends uvm_sequencer #(riscv_sequence_item);
-
+  //==================================================================================
+  // Registeration
+  //==================================================================================
   `uvm_component_utils(riscv_main_sequencer)
 
+  //==================================================================================
+  // Function: Constructor
+  //==================================================================================
   function new(string name = "riscv_main_sequencer", uvm_component parent = null);
     super.new(name, parent);
   endfunction
