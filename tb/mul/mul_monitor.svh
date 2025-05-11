5025e5f6f7cda0f8113b74a82b9e1939b2d24aeb
omar21hossam <62779504+omar21hossam@users.noreply.github.com>
2025-05-11 18:01:58 +0300

    Update mul_monitor.svh

--- tb/mul/mul_monitor.svh
+++ tb/mul/mul_monitor.svh
@@ -26,43 +26,53 @@ class mul_monitor extends uvm_monitor;
   // Connect phase
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
-    //@(posedge vif.clk);
     forever begin
-      @(posedge vif.ready_o);
+      @(posedge vif.clk );
       if (vif.rst_n == 0) begin
         seq_item.rst_n = 0;
         mon_analysis_port.write(seq_item);
       end
       else begin
         #1step;
-        seq_item.enable_i = vif.enable_i;
-        seq_item.operator_i = vif.operator_i;
+        wait(vif.enable_i);
+        if(vif.operator_i == riscv_pkg::MUL_MAC32) begin
+          #1step;
+        end
+        else if(vif.operator_i == riscv_pkg::MUL_H) begin
+          @(posedge vif.ready_o);
+          #1step;
+        end
+          seq_item.rst_n = vif.rst_n;
+          seq_item.enable_i = vif.enable_i;
+          seq_item.operator_i = vif.operator_i;
 
-        seq_item.short_signed_i = vif.short_signed_i;
-        seq_item.short_subword_i = vif.short_subword_i;
+          seq_item.short_signed_i = vif.short_signed_i;
+          seq_item.short_subword_i = vif.short_subword_i;
 
-        seq_item.operand_a_i = vif.operand_a_i;
-        seq_item.operand_b_i = vif.operand_b_i;
-        seq_item.operand_c_i = vif.operand_c_i;
-        seq_item.imm_i = vif.imm_i;
+          seq_item.operand_a_i = vif.operand_a_i;
+          seq_item.operand_b_i = vif.operand_b_i;
+          seq_item.operand_c_i = vif.operand_c_i;
+          seq_item.imm_i = vif.imm_i;
 
-        seq_item.dot_signed_i = vif.dot_signed_i;
-        seq_item.dot_op_a_i = vif.dot_op_a_i;
-        seq_item.dot_op_b_i = vif.dot_op_b_i;
-        seq_item.dot_op_c_i = vif.dot_op_c_i;
+          seq_item.dot_signed_i = vif.dot_signed_i;
+          seq_item.dot_op_a_i = vif.dot_op_a_i;
+          seq_item.dot_op_b_i = vif.dot_op_b_i;
+          seq_item.dot_op_c_i = vif.dot_op_c_i;
 
-        seq_item.is_clpx_i = vif.is_clpx_i;
-        seq_item.clpx_shift_i = vif.clpx_shift_i;
-        seq_item.clpx_img_i = vif.clpx_img_i;
+          seq_item.is_clpx_i = vif.is_clpx_i;
+          seq_item.clpx_shift_i = vif.clpx_shift_i;
+          seq_item.clpx_img_i = vif.clpx_img_i;
 
-        seq_item.ex_ready_i = vif.ex_ready_i;
-        seq_item.rst_n = vif.rst_n;
-        seq_item.result_o = vif.result_o;
-        seq_item.multicycle_o = vif.multicycle_o;
-        seq_item.ready_o = vif.ready_o;
-        seq_item.mulh_active_o = vif.mulh_active_o;
+          seq_item.ex_ready_i = vif.ex_ready_i;
+          seq_item.result_o = vif.result_o;
+          seq_item.multicycle_o = vif.multicycle_o;
+          seq_item.ready_o = vif.ready_o;
+          seq_item.mulh_active_o = vif.mulh_active_o;
+        // print sequence item
+        
         // Send the sequence item to the analysis port
         mon_analysis_port.write(seq_item); 
+        #1step;
       end
     end
   endtask
