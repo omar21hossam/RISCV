5a3ad191614b34db9783ba664225337067186739
mohamed11sayed <81857457+mohamed11sayed@users.noreply.github.com>
2025-05-17 06:04:53 +0200

    update cc class with all required corner cases scenarios

--- tb/prefetch/fetch_coverage_collector.svh
+++ tb/prefetch/fetch_coverage_collector.svh
@@ -109,19 +109,48 @@ bins  SW       = {riscv_pkg::SW};
             bins reg_val[] = {[0:31]};
         }
               
-        // // Compressed instruction coverage
-        // compressed: coverpoint seq_item_op.is_compressed_id_o {
-        //     bins regular    = {1'b0};
-        //     bins compressed = {1'b1};
-        // }
-        
-        // // Illegal instruction coverage
-        // illegal: coverpoint seq_item_op.illegal_c_insn_id_o {
-        //     bins legal   = {1'b0};
-        //     bins illegal = {1'b1};
-        // }
     endgroup
-    
+    covergroup tese_cases_scenarios;
+  direct_inst: coverpoint seq_item_ip.instr_rdata_i{
+         
+            bins Inst_handled_misalignment[] =(
+            'h0060006f => 'h00000013 => 'h006f0000 => 'h00000020 => 'h00000013 => 'h01260fb3
+        );
+            bins Instruction_ignored_misalignment[] = (
+            'h0010006f => 'h0030006f => 'h0050006f => 'h0070006f
+        );
+
+        bins div_corner_cases[] = (
+    'h000FA0B7 => 'hA2808093 =>  'h80000137 => 'h00010113 =>   'h0200C2B3  =>   'h0200E2B3 => 'h0200D2B3   => 'h0200F2B3 =>
+     'h023142B3   =>    'h023162B3               
+);
+
+        bins mul_corner_cases[] = (
+    'h00100093 => 'h80000137 =>  'hFFF10113 => 'h00AF41B7 =>   'hA4C18193  =>   'h020182B3 => 'h021182B3   => 'h022182B3 
+                  
+);
+
+
+bins alu_corner_cases[] = (
+    'h800000B7 => 'hFFF08093 =>  // li x1, 0x80000000 (max negative)
+    'h80000137 => 'h00010113 =>  // li x2, 0x80000000
+    'h01900193 =>                // li x3, 25
+    'hFCF00213 =>                // li x4, -49
+    'h001082B3 =>                // add x5, x1, x1 (max_neg + max_neg)
+    'h003082B3 =>                // add x5, x1, x3 (max_neg + pos)
+    'h004102B3 =>                // add x5, x2, x4 (max_neg + neg)
+    'h002082B3 =>        'h404082B3 =>    'h403102B3 =>    'h000002B3 =>    'h400002B3 =>    'h4001D2B3 =>   'h000192B3 =>  
+    'h0001D2B3 =>        'h41F1D293 =>    'h01F19293 =>    'h01F1D293 =>    'h00000263 =>     'h00001063           
+);
+
+
+
+        }
+
+
+
+
+    endgroup  
 //-------------------------------------------------------------------------------
 
 
@@ -129,6 +158,7 @@ bins  SW       = {riscv_pkg::SW};
   function new(string name = "fetch_coverage_collector", uvm_component parent = null);
     super.new(name, parent);
     op_instruction_cg = new();
+    tese_cases_scenarios = new();
   endfunction
 
   function void build_phase(uvm_phase phase);
@@ -158,6 +188,7 @@ bins  SW       = {riscv_pkg::SW};
   cov_fifo_op.get(seq_item_op);
   cov_fifo_ip.get(seq_item_ip);
         op_instruction_cg.sample();
+        tese_cases_scenarios.sample();
         ///direct sequences cover groups.
     end
   endtask
